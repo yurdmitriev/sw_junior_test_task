3ba56e8ddcbb4440bf94fc649c5085ab3d3deb24
yurdmitriev <yurdmitriev02@gmail.com>
2022-06-24 21:15:59 +0300

    Implement class autoload

--- app/api/index.php
+++ app/api/index.php
@@ -1,5 +1,15 @@
 <?php
 
+const NAMESPACE_ROOT = 'App';
+
+spl_autoload_register(function ($class) {
+    $class = str_replace(['\\', NAMESPACE_ROOT], [DIRECTORY_SEPARATOR, 'src'], $class);
+    $file = __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
+
+    if (file_exists($file))
+        include $file;
+});
+
 $url = parse_url($_SERVER['REQUEST_URI']);
 
 header("Content: application/json");
