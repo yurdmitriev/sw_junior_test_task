3bc9d43c9791eff59f5f49094c1cabfa22ebc102
yurdmitriev <yurdmitriev02@gmail.com>
2022-07-03 16:22:56 +0300

    Big update for sql builder class

--- app/api/src/Query.php
+++ app/api/src/Query.php
@@ -10,97 +10,195 @@ class Query {
     private string $command = '';
     private int $limit = 0;
     private int $offset = 0;
+    private array $fields = [];
+    private array $where = [];
+    private \PDOStatement $statement;
+    private array $params = [];
+    private string $orderColumn;
+    private string $orderDir = 'ASC';
 
     public function __construct(\PDO $pdo) {
         $this->pdo = $pdo;
     }
 
-    public function select(string $table, array $columns = []): self {
-        $fields = '*';
-
+    public function select(array $fields = []): self {
         if ($this->command) throw new Error();
         else $this->command = "SELECT";
 
-        if (!empty($columns)) {
-            $temp = [];
+        if (!empty($fields)) {
+            foreach ($fields as $table => $columns) {
+                $temp = [];
 
-            foreach ($columns as $column => $alias) {
-                if (is_numeric($column)) $temp[] = $alias;
-                else $temp[] = "$column as $alias";
+                if (empty($columns)) $temp = '*';
+                else foreach ($columns as $column => $alias) {
+                    if (is_numeric($column)) $temp[] = $alias;
+                    else $temp[] = "$column as $alias";
+                }
+
+                $this->fields[$table] = $temp;
             }
+        } else throw new Error();
 
-            $fields = implode(', ', $temp);
-        }
-
-        $this->sql .= "SELECT $fields FROM $table";
         return $this;
     }
 
     public function insert(string $table, array $fields): self {
-        foreach ($fields as $key => $value) {
-            if (!is_numeric($value)) $fields[$key] = "'$value'";
-        }
-
-        $columns = array_keys($fields);
-        $values = array_values($fields);
-
         if ($this->command) throw new Error();
         else $this->command = "INSERT";
 
-        $colString = implode(', ', $columns);
-        $valString = implode(', ', $values);
+        $temp = [];
 
-        $this->sql .= "INSERT INTO $table ($colString) VALUES ($valString)";
-
-        return $this;
-    }
-
-    public function join(string $table, string $on, string $base): self {
-        $this->sql .= " JOIN {$table} ON {$table}.{$on} = {$base}";
-        return $this;
-    }
-
-    public function where(array $filters): self {
-        if (!empty($filters)) {
-            $conditions = [];
-
-            foreach ($filters as $column => $value) {
-                $conditions[] = "$column = '$value'";
-            }
-
-            $this->sql .= " WHERE " . implode('AND ', $conditions);
+        foreach ($fields as $column => $value) {
+            $temp[$column] = ":$table$column";
+            $this->params[":$table$column"] = $value;
         }
 
+        $this->fields[$table] = $temp;
+
         return $this;
     }
 
-    public function limit(int $count = 0, int $offset = 0): self {
-        if ($count || $offset) $this->sql .= " LIMIT $offset, $count";
-        $this->limit = $count;
-        $this->offset = $offset;
+    public function where(string $table, string $column, $value, string $logical = "AND", string $operator = '='): self {
+        $temp = [];
+
+        if (is_array($value)) {
+            $temp['value'] = "$table.$column = {$value['bind']}.{$value['on']}";
+        } else {
+            $temp['value'] = "$table.$column $operator :$table$column";
+            $this->params[":$table$column"] = $value;
+        }
+
+        if (!isset($this->fields[$table])) $this->fields[$table] = null;
+
+        $temp['connects'] = empty($this->where) ? '' : $logical;
+        $this->where[] = $temp;
 
         return $this;
+    }
+
+    public function order($alias, string $dir = 'ASC'): self {
+        if (is_array($alias)) $this->orderColumn = implode(', ', $alias);
+        else $this->orderColumn = $alias;
+
+        return $this;
+    }
+
+    public function one(int $offset = 0) {
+        $this->limit = 1;
+        $this->offset = $offset;
+
+        $this->prepare();
+
+        return $this->run();
+    }
+
+    public function many(int $limit = 0, int $offset = 0) {
+        $this->limit = $limit;
+        $this->offset = $offset;
+
+        $this->prepare();
+
+        return $this->run();
     }
 
     public function raw(): string {
-        return $this->sql;
+        $this->prepare();
+
+        return $this->statement->queryString;
     }
 
-    public function run(int $mode = \PDO::FETCH_ASSOC, string $class = null) {
-        $query = $this->pdo->prepare($this->sql);
+    private function prepare(): void {
+        switch ($this->command) {
+            case "SELECT":
+                $fields = [];
+                $tables = [];
+
+                foreach ($this->fields as $table => $columns) {
+                    $tables[] = $table;
+
+                    if (is_array($columns))
+                        foreach ($columns as $column) {
+                            $fields[] = "$table.$column";
+                        }
+                    elseif ($columns !== null) $fields[] = "$table.$columns";
+                }
+
+                $fields = implode(', ', $fields);
+                $tables = implode(', ', $tables);
+                $this->sql = "SELECT $fields FROM $tables";
+
+                break;
+
+            case "INSERT":
+                $target = '';
+                $columns = [];
+                $values = [];
+
+                foreach ($this->fields as $table => $col) {
+                    $target = $table;
+
+                    foreach ($col as $key => $value) {
+                        $columns[] = $key;
+                        $values[] = $value;
+                    }
+
+                    break;
+                }
+
+                $values = implode(', ', $values);
+                $columns = implode(', ', $columns);
+
+                $this->sql = "INSERT INTO $target ($columns) VALUES ($values)";
+
+                break;
+
+            case "UPDATE":
+                // TODO: Implement an UPDATE method
+                break;
+
+            case "DELETE":
+                // TODO: Implement a DELETE method
+                break;
+        }
+
+        if (!empty($this->where)) {
+            $this->sql .= " WHERE";
+
+            foreach ($this->where as $condition) {
+                $this->sql .= " {$condition['connects']} {$condition['value']}";
+            }
+        }
+
+        if (!empty($this->orderColumn)) {
+            $this->sql .= " ORDER BY {$this->orderColumn} {$this->orderDir}";
+        }
+
+        if ($this->limit > 0) {
+            if ($this->offset > 0) $this->sql .= " LIMIT {$this->offset}, {$this->limit}";
+            else $this->sql .= " LIMIT {$this->limit}";
+        } else {
+            if ($this->offset > 0) $this->sql .= " OFFSET {$this->offset}";
+        }
+
+        $this->statement = $this->pdo->prepare($this->sql);
+    }
+
+    public function run(bool $dump = false) {
+        $this->prepare();
+
+        $executed = $this->statement->execute($this->params);
+        if ($dump) {
+            echo "<pre>";
+            $this->statement->debugDumpParams();
+            echo "</pre>";
+            exit();
+        }
 
         if ($this->command == 'SELECT') {
-            $query->execute();
-
-            if ($mode == \PDO::FETCH_CLASS) {
-                $result = $query->fetchAll($mode, $class);
-            } else {
-                $result = $query->fetchAll($mode);
-            }
-
+            $result = $this->statement->fetchAll(\PDO::FETCH_ASSOC);
             return $this->limit == 1 ? array_pop($result) : $result;
         } else {
-            return $query->execute();
+            return $executed;
         }
     }
 }
