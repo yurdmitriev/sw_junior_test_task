133fed5bda6b7e91f3373359982b081d731d8490
yurdmitriev <yurdmitriev02@gmail.com>
2022-07-06 14:35:41 +0300

    implement product card component

---
+++ app/html/src/components/ProductCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="item card">
+    <div class="card-body text-center">
+      <div class="form-check">
+        <input type="checkbox" name="delete" :value="sku" class="delete-checkbox form-check-input" @change="$emit('set', $event.target.checked, sku)">
+      </div>
+      <div class="sku">{{sku}}</div>
+      <div class="name">{{name}}</div>
+      <div class="price">{{price}}</div>
+      <div class="attribute">{{attribute}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  props: ['sku', 'name', 'price', 'attribute']
+}
+</script>
+
+<style scoped>
+
+</style>
