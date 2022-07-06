739014bab253f3e949cae12a634ecbb45a354296
yurdmitriev <yurdmitriev02@gmail.com>
2022-07-06 16:59:53 +0300

    Fix navbar

--- app/html/src/App.vue
+++ app/html/src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <header class="navbar navbar-light bg-white sticky-top p-3 border-bottom">
-    <div class="container g-1">
+    <div class="container flex-nowrap g-1">
       <h1 class="m-0">{{ $route.meta.title }}</h1>
       <div class="buttons d-flex gap-2" v-if="$route.name === 'home'">
-        <button type="button" class="btn btn-primary" @click="$router.push('/add-product')">Add</button>
-        <button type="button" class="btn btn-danger" :disabled="store.selected.length === 0">Mass delete</button>
+        <button type="button" class="btn btn-primary" @click="$router.push('/add-product')">ADD</button>
+        <button type="button" class="btn btn-danger" :disabled="store.selected.length === 0">MASS DELETE</button>
       </div>
       <div class="buttons d-flex gap-2" v-if="$route.name === 'add'">
-        <button type="button" class="btn btn-primary" @click="submitForm">Add</button>
-        <button type="button" class="btn btn-danger" @click="$router.push('/')">Cancel</button>
+        <button type="button" class="btn btn-primary" @click="submitForm">ADD</button>
+        <button type="button" class="btn btn-danger" @click="$router.push('/')">CANCEL</button>
       </div>
     </div>
   </header>
@@ -39,5 +39,11 @@ export default {
   min-height: 100vh;
   display: flex;
   flex-direction: column;
+}
+
+header h1 {
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 }
 </style>
