3e3afa35b9dddbeaf274d6eac318a2d82a912a90
yurdmitriev <yurdmitriev02@gmail.com>
2022-07-07 13:55:03 +0300

    Load list of products from local domain

--- app/html/src/views/HomeView.vue
+++ app/html/src/views/HomeView.vue
@@ -1,30 +1,8 @@
 <template>
   <main class="home">
     <section class="list row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3 g-lg-4">
-      <div class="col">
-        <product-card sku="test" name="Testing product" price="1500 $" attribute="Dimensions: 10x10x5"
-                      @set="listenCheckbox"/>
-      </div><div class="col">
-        <product-card sku="test" name="Testing product" price="1500 $" attribute="Dimensions: 10x10x5"
-                      @set="listenCheckbox"/>
-      </div><div class="col">
-        <product-card sku="test" name="Testing product" price="1500 $" attribute="Dimensions: 10x10x5"
-                      @set="listenCheckbox"/>
-      </div><div class="col">
-        <product-card sku="test" name="Testing product" price="1500 $" attribute="Dimensions: 10x10x5"
-                      @set="listenCheckbox"/>
-      </div><div class="col">
-        <product-card sku="test" name="Testing product" price="1500 $" attribute="Dimensions: 10x10x5"
-                      @set="listenCheckbox"/>
-      </div><div class="col">
-        <product-card sku="test" name="Testing product" price="1500 $" attribute="Dimensions: 10x10x5"
-                      @set="listenCheckbox"/>
-      </div><div class="col">
-        <product-card sku="test" name="Testing product" price="1500 $" attribute="Dimensions: 10x10x5"
-                      @set="listenCheckbox"/>
-      </div>
-      <div class="col">
-        <product-card sku="test" name="Testing product" price="1500 $" attribute="Dimensions: 10x10x5"
+      <div class="col" v-for="product in products" :key="product.sku">
+        <product-card :sku="product.sku" :name="product.name" :price="product.price" :attribute="product.attribute"
                       @set="listenCheckbox"/>
       </div>
     </section>
@@ -40,7 +18,8 @@ export default {
   components: {ProductCard},
   data() {
     return {
-      store
+      store,
+      products: []
     }
   },
   methods: {
@@ -50,7 +29,16 @@ export default {
         let index = this.store.selected.indexOf(sku);
         if (index > -1) this.store.selected.splice(index, 1);
       }
+    },
+    getProducts() {
+      fetch('http://products-app.local/api/products')
+          .then(response => response.json())
+          .then(data => this.products = data.data)
+          .catch(e => console.log(e));
     }
+  },
+  beforeMount() {
+    this.getProducts()
   },
   unmounted() {
     this.store.selected = [];
