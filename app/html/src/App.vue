c60a8511f7fc35dca194e482b5148f326fd098ff
yurdmitriev <yurdmitriev02@gmail.com>
2022-07-07 18:53:51 +0300

    Implement form submitting

--- app/html/src/App.vue
+++ app/html/src/App.vue
@@ -14,7 +14,7 @@
         <button type="button" class="btn btn-primary" @click="submitForm">
           <i class="bi bi-cloud-download d-sm-none"></i><span class="d-none d-sm-inline">ADD</span>
         </button>
-        <button type="button" class="btn btn-danger" @click="$router.push('/')">
+        <button type="button" class="btn btn-danger" @click="cancel">
           <i class="bi bi-x-circle d-sm-none"></i><span class="d-none d-sm-inline">CANCEL</span>
         </button>
       </div>
@@ -24,10 +24,18 @@
   <footer class="text-center p-3 border-top mt-auto">
     <p>Scandiweb Test assignment</p>
   </footer>
+  <div class="toast-container position-fixed top-0 end-0 p-3" style="z-index: 9999">
+    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" ref="toast">
+      <div class="toast-body" ref="toastBody">
+        There must be an alert message
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import {store} from "@/store";
+import {Toast} from "bootstrap";
 
 export default {
   data() {
@@ -36,7 +44,47 @@ export default {
     }
   },
   methods: {
+    cancel() {
+      const form = document.querySelector('#product_form');
+      if (form) form.reset();
+      this.$router.push('/')
+    },
     submitForm() {
+      const form = document.querySelector('#product_form');
+      const data = new FormData(form);
+      const toast = new Toast(this.$refs.toast, {delay: 2000});
+
+      this.$refs.toast.addEventListener('hidden.bs.toast', function (e) {
+        e.target.className = 'toast';
+      })
+
+      for (let [key, value] of data) {
+        if (value) continue;
+
+        document.querySelector(`#${key}`).classList.add('is-invalid')
+      }
+
+      if (form.checkValidity()) {
+        fetch('http://products-app.local/api/products', {
+          method: 'POST',
+          body: data
+        })
+            .then(response => {
+              if (response.ok) {
+                form.reset();
+                this.$router.push('/');
+                this.$refs.toast.classList.add('bg-success', 'text-white');
+              } else {
+                this.$refs.toast.classList.add('bg-danger', 'text-white');
+              }
+              return response.json();
+            })
+            .then(json => {
+              console.log(json);
+              this.$refs.toastBody.innerText = json.message;
+              toast.show();
+            });
+      }
     }
   }
 }
